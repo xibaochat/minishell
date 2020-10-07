ad977bf89f75b08dfebd4915f42d076d099ebd3a
xibaochat <you@example.com>
2020-10-07 15:51:25 +0200

    Add commentary + optimize

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,9 @@
 #include "minishell.h"
 
+/**
+ * Replace given var in sh->env by an empty string
+ */
+
 void unset(char **arr, t_mini *sh)
 {
 	int i;
@@ -11,9 +15,9 @@ void unset(char **arr, t_mini *sh)
 	while (arr[i])
 	{
 		j = 0;
+		lens = ft_strlen(arr[i]);
 		while (sh->env[j])
 		{
-			lens = ft_strlen(arr[i]);
 			if (!ft_strncmp(arr[i], sh->env[j], lens)
 				&& sh->env[j][lens] && sh->env[j][lens] == '=')
 			{
