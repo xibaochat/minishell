2a92bd58c39ecbc3910e547ce87dde22258a829e
xibaochat <you@example.com>
2020-10-18 15:36:57 +0200

    Add is_unescapted_c func

--- libft/src/escapted_char_management.c
+++ libft/src/escapted_char_management.c
@@ -10,3 +10,16 @@ int is_escapted(t_quo *q, const char *s, int i)
 		++nb_bslash;
 	return (nb_bslash % 2);
 }
+
+/**
+ * Check if current char (=s[i]) is equal to given char (=c)
+ * If so, also check that the char is not escaped
+*/
+int	is_unescapted_c(t_quo *q, const char *s, int i, const char c)
+{
+	if (s[i] != c)
+		return (0);
+	if (is_escapted(q, s, i))
+		return (0);
+	return (1);
+}
