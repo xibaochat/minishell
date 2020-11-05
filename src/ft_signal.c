8167cd7448e96c45698df959b5ca7b0b7f5d14e5
xibaochat <xinwang@student.42.fr>
2020-11-05 16:36:14 +0100

    ctrl_slash

--- src/ft_signal.c
+++ src/ft_signal.c
@@ -1,14 +1,28 @@
 # include "minishell.h"
 
+void ft_cold_quit(int sig)
+{
+	t_mini **sh;
+
+	sh = get_sh();
+	if ((*sh)->is_cmd == 1)
+	{
+		(*sh)->last_return = 131;
+		ft_putstr_w_new_line_fd("", 2);
+	}
+	else
+		(*sh)->last_return = 0;
+}
 
 void ft_not_quit(int sig)
 {
 	char **buffer;
 	t_mini **sh;
 
-//empty gnl buffer in case we used ctrl_D to provide a eof
+//empty gnl buffer in case we use ctrl_D to provide a EOF
 	buffer = get_buffer();
 	sh = get_sh();
+	(*sh)->last_return = 130;
 	if (*buffer)
 	{
 		free(*buffer);
@@ -17,7 +31,6 @@ void ft_not_quit(int sig)
 	ft_putstr_w_new_line_fd("", 2);
 	if ((*sh)->is_cmd) // ls -R / in the process of execution
 	{
-		(*sh)->last_return = 130;
 		print_prompt(*sh);
 		if ((*sh)->ctrl_c == 0)
 			(*sh)->ctrl_c = 1;
@@ -30,4 +43,5 @@ void ft_signal(t_mini *sh)
 {
 //receive ctrl-C signal, works like mlx_hook
 	signal(SIGINT, ft_not_quit);
+	signal(SIGQUIT, ft_cold_quit);
 }
