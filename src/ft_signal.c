32e22bce1ad20d8682266f02dda239272f4b7cfa
xibaochat <xinwang@student.42.fr>
2020-11-07 04:04:10 +0100

    add signal

--- src/ft_signal.c
+++ src/ft_signal.c
@@ -1,5 +1,18 @@
 # include "minishell.h"
 
+void ft_cold_quit(int sig)
+{
+	t_mini **sh;
+
+	sh = get_sh();
+	if ((*sh)->is_cmd == 1)
+	{
+		(*sh)->last_return = 131;
+		ft_putstr_w_new_line_fd("", 2);
+	}
+	else
+		(*sh)->last_return = 0;
+}
 
 void ft_not_quit(int sig)
 {
@@ -15,19 +28,20 @@ void ft_not_quit(int sig)
 		*buffer = NULL;
 	}// jusqu'a ici
 	ft_putstr_w_new_line_fd("", 2);
-	if ((*sh)->is_cmd) // ls -R / in the process of execution
-	{
-		(*sh)->last_return = 130;
+	(*sh)->last_return = 130;
+//	if ((*sh)->is_cmd) // ls -R / in the process of execution
+//	{
 		print_prompt(*sh);
-		if ((*sh)->ctrl_c == 0)
-			(*sh)->ctrl_c = 1;
-	}
-	else
-		print_prompt(*sh);
+//		if ((*sh)->ctrl_c == 0)
+//			(*sh)->ctrl_c = 1;
+//	}
+//	else
+//		print_prompt(*sh);
 }
 
 void ft_signal(t_mini *sh)
 {
 //receive ctrl-C signal, works like mlx_hook
 	signal(SIGINT, ft_not_quit);
+	signal(SIGQUIT, ft_cold_quit);
 }
