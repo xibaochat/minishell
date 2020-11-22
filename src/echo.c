a1f3e654754b2d36b5ebd0462e2ea00955c43c05
xibaochat <xinwang@student.42.fr>
2020-11-22 21:02:39 +0100

    fix echo -n

--- src/echo.c
+++ src/echo.c
@@ -4,11 +4,24 @@
 ** The "-n" option skips the '\n' after executing echo
 */
 
+static int	str_is_all_n(char *s)
+{
+	int	i;
+
+	i = 1;
+	while (s[i] && s[i] == 'n')
+		i++;
+	if (!s[i])
+		return (1);
+	else
+		return (0);
+}
+
 int	has_n_option(char *s)
 {
 	if (s)
 	{
-		if (s[0] == '-' && s[1] == 'n' && ft_strlen(s) == 2)
+		if (s[0] == '-' && s[1] == 'n' && str_is_all_n(s))
 			return (1);
 	}
 	return (0);
@@ -17,19 +30,26 @@ int	has_n_option(char *s)
 void	echo(char **args)
 {
 	int	i;
+	int	mark;
+	int	has_n;
 
-	i = has_n_option(args[1]);
+	mark = 0;
+	i = 0;
 	while (args[++i])
 	{
+		while (has_n_option(args[i]) && !mark)
+		{
+			has_n = 1;
+			i++;
+		}
 		if (args[i] && args[i][0])
 		{
-//			ft_putstr(args[i]);
 			ft_putstr_echo(args[i]);
 			if (args[i + 1] && args[i + 1][0])
 				ft_putstr(" ");
+			mark = 1;
 		}
-		// don't show str if it is empty echo "" v1 v2
 	}
-	if (!has_n_option(args[1]))
+	if (has_n)
 		ft_putstr("\n");
 }
