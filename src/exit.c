ed2e53d50ea6a0554161ae8598a9d82115f17abe
xibaochat <xinwang@student.42.fr>
2020-11-22 22:20:05 +0100

    exit arg

--- src/exit.c
+++ src/exit.c
@@ -9,24 +9,29 @@ static int	has_alpha(char *s)
 	{
 		if (ft_isalpha(s[i]))
 			return (1);
+		if (i > 0 && ((s[i] == '+' || s[i] == '-') || !ft_isalpha(s[i])))
+			return (1);
 	}
 	return (0);
 }
 
-static void	ft_check_exit(char **s, t_mini *sh)
+static void	fst_arg_is_alpha(char *s, t_mini *sh)
 {
-	if (has_alpha(s[1]))
+	if (s && has_alpha(s))
 	{
-		ft_printf("exit\n./minishell: exit: %s : ", s[1]);
+		ft_printf("exit\n./minishell: exit: %s : ", s);
 		ft_printf("numeric argument required\n");
 		ft_tabfree(sh->env);
 		exit(255);
 	}
-	else
-	{
-		ft_tabfree(sh->env);
-		exit(ft_atoi(s[1]));
-	}
+}
+
+static void	ft_check_exit(char **s, t_mini *sh)
+{
+	fst_arg_is_alpha(s[1], sh);
+	ft_tabfree(sh->env);
+	ft_putstr("exit\n");
+	exit(ft_atoi(s[1]));
 }
 
 void	ft_exit(char **split_input, t_mini *sh)
@@ -36,7 +41,8 @@ void	ft_exit(char **split_input, t_mini *sh)
 	tab_len = ft_tablen(split_input);
 	if (tab_len > 2)
 	{
-		ft_putstr_fd("./Minishell: exit: too many arguments\n", 2);
+		fst_arg_is_alpha(split_input[1], sh);
+		ft_putstr_fd("./minishell: exit: too many arguments\n", 2);
 		sh->last_return = 1;
 	}
 	else if (tab_len == 2)
