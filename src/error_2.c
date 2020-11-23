28cf25b5c7413c1bf8a4d338878fcca4f1ac237a
Padoue <padoue.nielly@gmail.com>
2020-11-23 16:58:47 +0100

    trim + ;

--- src/error_2.c
+++ src/error_2.c
@@ -1,32 +1,33 @@
 #include "minishell.h"
 
+int	cmd_exists(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] && s[i] != ';' && !ft_strchr(SPACE, s[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 int	empty_cmd(char **arr)
 {
 	int	i;
-	int	j;
-	int	empty;
 
 	i = 0;
-	empty = 1;
 	while (arr[i])
 	{
-		j = 0;
-		empty = 1;
-		while (arr[i][j])
+		if (!cmd_exists(arr[i]))
 		{
-			if (arr[i][j] && arr[i][j] != ';' && !ft_strchr(SPACE, arr[i][j]))
-				empty = 0;
-			j++;
+			ft_printf("syntax error near unexpected token ';'\n");
+			ft_tabfree(arr);
+			return (1);
 		}
 		i++;
-		if (empty)
-			break ;
-	}
-	if (empty)
-	{
-		ft_printf("syntax error near unexpected token ';'\n");
-		ft_tabfree(arr);
-		return (1);
 	}
 	return (0);
 }
