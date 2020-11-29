c2daafbfa169058c8aec72103cbd8ba15fe2fb61
xibaochat <wmmxxbb@gmail.com>
2020-11-29 11:03:44 +0100

    fix merge redir

---
+++ manage_loney_redir_char_in_arr.c
@@ -0,0 +1,75 @@
+#	include "minishell.h"
+
+int lonely_redir_char(char *str)
+{
+	if ((str[0] == '>' || str[0] == '<') && ft_strlen(str) == 1)
+		return (1);
+	else if (str[0] && str[1] &&
+			 str[0] == '>' && str[1] == '>' && ft_strlen(str) == 2)
+		return (1);
+	return (0);
+}
+
+void merge_two_args(char ***arr, int index)
+{
+	char **new_arr;
+	int i;
+	int lens;
+
+	i = -1;
+	lens = ft_tablen(*arr);
+	new_arr = NULL;
+	new_arr = (char **)malloc(sizeof(char *) * (lens));
+	while (++i < lens)
+		new_arr[i] = NULL;
+	i = -1;
+	while (++i <= index)
+		new_arr[i] = ft_strdup((*arr)[i]);
+	index += 2;
+	while ((*arr)[index])
+	{
+		new_arr[i] = ft_strdup((*arr)[index]);
+		index++;
+		i++;
+	}
+	*arr = new_arr;
+}
+
+int merge_str_and_create_new_arr(char ***arr, int i)
+{
+	char *tmp;
+
+	tmp  = NULL;
+	tmp = ft_strjoin((*arr)[i] , (*arr)[i + 1]);
+	if (valid_red_char_combinaison(tmp, 0))
+	{
+		free_str((*arr)[i]);
+		(*arr)[i] = tmp;
+		merge_two_args(arr, i);
+		return (1);
+	}
+	else
+	{
+		free_str(tmp);
+		redirection_message_err((*arr)[i][0]);
+		return (-1);
+	}
+}
+
+int manage_lonely_redir_char(int i, char ***arr)
+{
+
+	int res;
+
+	res = -1;
+	if ((*arr)[i + 1])
+	{
+		res = merge_str_and_create_new_arr(arr, i);
+		if (res == -1)
+			return (res);
+		return (1);
+	}
+	else
+		redirection_message_err_2();
+	return (res);
+}
--- maobe_check_for_redir.c
+++ maobe_check_for_redir.c
@@ -19,14 +19,10 @@ typedef struct  t_red
 }               t_red;
 
 int jump_to_redir_char(const char *cmd, int *i);
-int redirection_message_err(char c);
-int redirection_message_err_2();
-int merge_str_and_create_new_arr(char ***arr, int i);
-int valid_red_char_combinaison(char *cmd, int i);
 int get_filename_len(const char *cmd, int i);
 int jump_i_to_filename(char *str, int i);
-int is_redir_char(const char c);
-int redir_in_str(const char *s);
+
+
 
 t_red init_red_struct(void)
 {
@@ -38,97 +34,6 @@ t_red init_red_struct(void)
     s.p[0] = -1;
     s.p[1] = -1;
     return (s);
-}
-
-int lonely_redir_char(char *str)
-{
-	if ((str[0] == '>' || str[0] == '<') && ft_strlen(str) == 1)
-		return (1);
-	else if (str[0] && str[1] &&
-			 str[0] == '>' && str[1] == '>' && ft_strlen(str) == 2)
-		return (1);
-	return (0);
-}
-
-void    ft_tabfree(char **tab)
-{
-    int i;
-
-    i = -1;
-    if (tab)
-    {
-        while (tab[++i])
-            free_str(tab[i]);
-        free(tab);
-        tab = NULL;
-    }
-}
-
-void    show_arr_value(char **arr)
-{
-    int i;
-
-    i = -1;
-    if (arr)
-    {
-        while (arr[++i])
-            ft_putstr_w_new_line(arr[i]);
-    }
-}
-
-int  tablen(char **tab)
-{
-    int len;
-
-    len = 0;
-    while (tab[len] != NULL)
-        len++;
-    return (len);
-}
-
-void merge_two_args(char ***arr, int index)
-{
-	char **new_arr;
-	int i;
-	int lens;
-
-	i = -1;
-	lens = tablen(*arr);
-	new_arr = NULL;
-	new_arr = (char **)malloc(sizeof(char *) * (lens));
-	while (++i < lens)
-		new_arr[i] = NULL;
-	i = -1;
-	while (++i <= index)
-		new_arr[i] = ft_strdup((*arr)[i]);
-	index += 2;
-	while ((*arr)[index])
-	{
-		new_arr[i] = ft_strdup((*arr)[index]);
-		index++;
-		i++;
-	}
-	*arr = new_arr;
-}
-
-int merge_str_and_create_new_arr(char ***arr, int i)
-{
-	char *tmp;
-
-	tmp  = NULL;
-	tmp = ft_strjoin((*arr)[i] , (*arr)[i + 1]);
-	if (valid_red_char_combinaison(tmp, 0))
-	{
-		free_str((*arr)[i]);
-		(*arr)[i] = tmp;
-		merge_two_args(arr, i);
-		return (1);
-	}
-	else
-	{
-		free_str(tmp);
-		return (-1);
-	}
 }
 
 void set_red_type(char *str, int i, t_red *red)
@@ -267,22 +172,17 @@ int manage_redir(char **str)
 			return (-1);
 		}
 		remove_redir_from_cmd(&red, str, i);
-		ft_putstr("\nIn the function manage_redir, str is \n");
 	}
 	return (0);
 
 }
 
-//int main(void)
 char ** merge_redir_str_and_redirection(char **arr, t_mini *sh)
 {
 	int i;
-	int res;
 	int redir_res;
 
-
 	i = -1;
-	res = 0;
 	redir_res = 0;
 	while (arr[++i])
 	{
@@ -290,68 +190,38 @@ char ** merge_redir_str_and_redirection(char **arr, t_mini *sh)
 		{
 			if (lonely_redir_char(arr[i]))
 			{
-				if (arr[i + 1])
-				{
-					res = merge_str_and_create_new_arr(&arr, i);
-					if (res == -1)
-					{
-						ft_putstr("je suis boid1\n");
-						redirection_message_err(arr[i + 1][0]);
-						sh->last_return  = 2;
-						return (NULL);
-					}
-					else
-					{
-						redir_res = manage_redir(&arr[i]);
-						if (redir_res == -1)
-						{
-							ft_putstr("je suis boid2\n");
-							sh->last_return = 2;
-							return (NULL);
-						}
-					}
-				}
-				else
-				{
-					ft_putstr("je suis boid3\n");
-					redirection_message_err_2();
-					sh->last_return  = 2;
+				if (manage_lonely_redir_char(i, &arr) == -1)
 					return (NULL);
-				}
 			}
-			else
+			redir_res = manage_redir(&arr[i]);
+			if (redir_res == -1)
 			{
-				redir_res = manage_redir(&arr[i]);
-				if (redir_res == -1)
-				{
-					redirection_message_err(arr[i + 1][0]);
-					sh->last_return  = 2;
-					return (NULL);
-				}
+				redirection_message_err(arr[i + 1][0]);
+				sh->last_return  = 2;
+				return (NULL);
 			}
 		}
 	}
 	return (arr);
 }
 
-
 char **maobe_check_for_redir(char **arr, t_mini *sh)
 {
 	int i;
-	char **new_arr0;
+	char **merge_arr;
 	int lens;
 	char **new_arr;
 
-	new_arr0 = merge_redir_str_and_redirection(arr, sh);
-	if (new_arr0)
+	merge_arr = merge_redir_str_and_redirection(arr, sh);
+	if (merge_arr)
 	{
 		lens = 0;
 		i = 0;
 		new_arr = NULL;
 		lens = 0;
-		while (new_arr0[i])
+		while (merge_arr[i])
 		{
-			if (new_arr0[i][0] != 2)
+			if (merge_arr[i][0] != 2)
 				lens++;
 			i++;
 		}
@@ -361,14 +231,11 @@ char **maobe_check_for_redir(char **arr, t_mini *sh)
 			new_arr[i] = NULL;
 		i = -1;
 		int j = 0;
-		while (new_arr0[++i])
+		while (merge_arr[++i])
 		{
-			if (new_arr0[i][0] != 2)
-				new_arr[j++] = new_arr0[i];
+			if (merge_arr[i][0] != 2)
+				new_arr[j++] = merge_arr[i];
 		}
-		ft_putstr("------------------------\n");
-		show_arr_value(new_arr);
-		ft_putstr("------------------------\n");
 		return(new_arr);
 	}
 	return (NULL);
