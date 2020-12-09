2965391b651f4a7d4592bc1761d1dd1f930e723a
NIELLY <padoue.nielly@gmail.com>
2020-12-09 18:34:59 +0100

    pipe fixed (needs testing)

--- manage_pipe.c
+++ manage_pipe.c
@@ -1,14 +1,14 @@
 #include "minishell.h"
 #include <stdio.h>
 
-void	pipe_child(int *fd, char **arr, int i, t_mini *sh)
+void	pipe_child(int *fd, char **arr, int i, t_mini *sh, int fd_in)
 {
 	int	nb_pipes;
 	int	last_ret;
 
+	dup2(fd_in, STDIN_FILENO);
 	nb_pipes = ft_tablen(arr) - 1;
 	close(fd[0]);
-	printf("child0\n");
 	if (i != nb_pipes)
 	{
 		if (dup2(fd[1], 1) == -1)
@@ -19,14 +19,11 @@ void	pipe_child(int *fd, char **arr, int i, t_mini *sh)
 		fd[0] = 0;
 		fd[1] = 1;
 	}
-	printf("child1\n");
 	last_ret = split_and_execute(arr[i], " ", 0, sh);
 	write(sh->p[1], &last_ret, sizeof(int));
 	close(sh->p[1]);
 	read(sh->p[0], &last_ret, sizeof(int));
 	close(sh->p[0]);
-	printf("child2\n");
-	write(1, "EXIT cHIIIIIIIIILD\n", 20);
 	close(fd[1]);
 	exit(1);
 }
@@ -39,15 +36,16 @@ int	pipe_parent(t_mini *sh, int *fd, int i, int nb_pipes)
 	close(sh->p[1]);
 	close(sh->p[0]);
 	close(fd[1]);
-	if (i != nb_pipes)
+/*	if (i != nb_pipes)
 	{
 		if (dup2(fd[0], 0) == -1)
 			ft_error("DUP2 FAILED", errno);
-	}
+	}*/
 	close(fd[0]);
-	printf("parent\n");
 	return (last_ret);
 	(void)sh;
+	(void)i;
+	(void)nb_pipes;
 }
 
 void	ft_piping(t_mini *sh, int *fd)
@@ -67,11 +65,13 @@ int	ft_manage_pipe(t_mini *sh, char	**arr)
 	int	last_ret;
 	int	status;
 	int	*tab;
+	int	fd_in;
 
 	last_ret = 0;
 	nb_pipes = ft_tablen(arr) - 1;
 	tab = malloc(sizeof(int) * (nb_pipes + 1));
 	tab[nb_pipes] = 0;
+	fd_in = dup(STDIN_FILENO);
 	i = -1;
 	while (++i <= nb_pipes)
 	{
@@ -79,21 +79,19 @@ int	ft_manage_pipe(t_mini *sh, char	**arr)
 		if (sh->last_pid < 0)
 			ft_error("FORK FAILED", errno);
 		else if (!sh->last_pid)
-			pipe_child(fd, arr, i, sh);
+			pipe_child(fd, arr, i, sh, fd_in);
 		else
 		{
+			close(fd_in);
+			fd_in = dup(fd[0]);
 			last_ret = pipe_parent(sh, fd, i, nb_pipes);
 			tab[i] = sh->last_pid;
 		}
-		printf("on passe au suivant \n");
 	}
 	i = -1;
 	while (++i <= nb_pipes)
-	{
-		printf("i = %d\n", i);
 		waitpid(tab[i], &status, 0);
-		printf("i = %d done\n", i);
-	}
+	close(fd_in);
 	free(tab);
 	return (last_ret);
 }
