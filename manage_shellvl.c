f6f82de34082b77ffd569c27762486d0fdbb6ace
xibaochat <wmmxxbb@gmail.com>
2020-12-14 17:29:39 +0100

    shell

--- manage_shellvl.c
+++ manage_shellvl.c
@@ -12,6 +12,20 @@
 
 #include "minishell.h"
 
+int		ft_strisdigit(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	manage_shellvl(t_mini *sh)
 {
 	char	*value;
@@ -22,12 +36,17 @@ void	manage_shellvl(t_mini *sh)
 	if (!sh->has_env_i)
 	{
 		value = ft_find_env("SHLVL=", sh->env);
+		if (!value[0] || (value[0] == '=' && !value[1]))
+			nb = 1;
 		if (ft_atoi(value) < 0 || ft_atoi(value) >= 999)
 		{
 			if (ft_atoi(value) >= 999)
 				ft_putstr_fd("shell level (1000) too high, resetting to 1\n", STDERR_FILENO);
 			nb = 0;
 		}
+		else if (value && (value[0] && value[0] != '+' &&  value[0] != '-')
+				 && (value + 1) && !ft_strisdigit(value + 1))
+			nb = 1;
 		else
 			nb = ft_atoi(value) + 1;
 		nb_str = ft_itoa(nb);
